4345f5cc98effa8848db5073ad52a3807d337d10
morioka <morioka@yahoo.co.jp>
2014-12-20 15:15:56 +0900

    readdir --> readdirSync

--- node/lesson03.js
+++ node/lesson03.js
@@ -8,30 +8,24 @@ var path   = require('path');
  * @param action function for an matched file
  */
 function findFiles(parent, filter, action) {
-	fs.readdir(parent, function(err, files) {
-		// Error
-		if (err) {
-			console.error(err);
-			return;
-		}
+	var files = fs.readdirSync(parent);
 
-		files.forEach(function(file) {
-			var child = path.join(parent, file);
-			var stats = fs.statSync(child);
-			if (stats.isDirectory()) {
-				findFiles(child, filter, action);
-			} else if (stats.isFile() && filter(file)) {
-				action(child);
-			}
-		});
+	files.forEach(function(file) {
+		var child = path.join(parent, file);
+		var stats = fs.statSync(child);
+		if (stats.isDirectory()) {
+			findFiles(child, filter, action);
+		} else if (stats.isFile() && filter(file)) {
+			action(child);
+		}
 	});
 }
 
 findFiles(
-	".",
+	"..",
 	function(file) {
 		return /.*\.js$/.test(file);
 	},
 	function(path) {
-		console.log("JavaScript: " + path)
+		console.log(path);
 	});
